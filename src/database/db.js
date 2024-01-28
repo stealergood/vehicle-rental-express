02cb34dbcbbfc9b4e269769e71bcee544927b4ca
Denny Edy Saputra <ncstealer@gmail.com>
2024-01-28 15:55:59 +0700

    Changes to be committed: 	modified:   src/database/db.js

--- src/database/db.js
+++ src/database/db.js
@@ -1,10 +1,19 @@
 const mysql = require("mysql2");
 
+const DB_URL = process.env.JAWSDB_URL;
+
+const extract = DB_URL.match(
+  /mysql:\/\/(?<user>\w+):(?<password>\w+)@(?<host>\w+):(?<port>\w+)\/(?<database>\w+)/
+);
+
+const { user, password, host, port, database } = extract.groups;
+
 const db = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.UNAME,
-  password: process.env.PASS,
-  database: process.env.DB,
+  host: host,
+  user: user,
+  password: password,
+  database: database,
+  port: port,
 });
 
 module.exports = db;
